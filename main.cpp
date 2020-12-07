b393560cc92f3e5fd151bf8fe035b61c29e6ddd5
Lukian <lukianbogdanets@gmail.com>
2020-12-07 15:51:43 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <string>
 #include <chrono>
-#include <cstdint>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 #include "Vehicle.h"
 
 using namespace std;
 
-void readFile(string outFile, vector<Vehicle*>& listings) {
+vector<Vehicle*> readFile(string outFile, vector<Vehicle*>& listings) {
     ifstream file;
 
     string make;
@@ -40,106 +41,107 @@ void readFile(string outFile, vector<Vehicle*>& listings) {
         getline(file, reliability,'\n');
 
 
-        Vehicle car = Vehicle(make, model, transmission, color, engine_type, body_type, stoi(odometer), stoi(year), stoi(price), stoi(reliability));
-        listings.push_back(&car);
+        Vehicle* car = new Vehicle(make, model, transmission, color, engine_type, body_type, stoi(odometer), stoi(year), stoi(price), stod(reliability));
+        listings.push_back(car);
     }
     file.close();
+    return listings;
 }
 
 void merge(vector<Vehicle*>& listings, int size, int low, int middle, int high, string criteria) {
-        vector<Vehicle*> temp(size);
-        for (int i = low; i <= high; i++) {
-            temp[i] = listings[i];
-        }
-        int i = low;
-        int j = middle + 1;
-        int k = low;
-        if (criteria == "Price") {
-            while (i <= middle && j <= high) {
-                if (temp[i]->Price <= temp[j]->Price) {
-                    listings[k] = temp[i];
-                    ++i;
-                } else {
-                    listings[k] = temp[j];
-                    ++j;
-                }
-                ++k;
-            }
-            while (i <= middle) {
+    vector<Vehicle*> temp(size);
+    for (int i = low; i <= high; i++) {
+        temp[i] = listings[i];
+    }
+    int i = low;
+    int j = middle + 1;
+    int k = low;
+    if (criteria == "Price") {
+        while (i <= middle && j <= high) {
+            if (temp[i]->Price <= temp[j]->Price) {
                 listings[k] = temp[i];
-                ++k;
                 ++i;
+            } else {
+                listings[k] = temp[j];
+                ++j;
             }
+            ++k;
         }
-        if (criteria == "Odometer") {
-            while (i <= middle && j <= high) {
-                if (temp[i]->Odometer <= temp[j]->Odometer) {
-                    listings[k] = temp[i];
-                    ++i;
-                } else {
-                    listings[k] = temp[j];
-                    ++j;
-                }
-                ++k;
-            }
-            while (i <= middle) {
-                listings[k] = temp[i];
-                ++k;
-                ++i;
-            }
-        }
-        if (criteria == "Year") {
-            while (i <= middle && j <= high) {
-                if (temp[i]->Year <= temp[j]->Year) {
-                    listings[k] = temp[i];
-                    ++i;
-                } else {
-                    listings[k] = temp[j];
-                    ++j;
-                }
-                ++k;
-            }
-            while (i <= middle) {
-                listings[k] = temp[i];
-                ++k;
-                ++i;
-            }
-        }
-        if (criteria == "Reliability") {
-            while (i <= middle && j <= high) {
-                if (temp[i]->Reliability <= temp[j]->Reliability) {
-                    listings[k] = temp[i];
-                    ++i;
-                } else {
-                    listings[k] = temp[j];
-                    ++j;
-                }
-                ++k;
-            }
-            while (i <= middle) {
-                listings[k] = temp[i];
-                ++k;
-                ++i;
-            }
-        }
-        if (criteria == "Rank") {
-            while (i <= middle && j <= high) {
-                if (temp[i]->Rank <= temp[j]->Rank) {
-                    listings[k] = temp[i];
-                    ++i;
-                } else {
-                    listings[k] = temp[j];
-                    ++j;
-                }
-                ++k;
-            }
-            while (i <= middle) {
-                listings[k] = temp[i];
-                ++k;
-                ++i;
-            }
+        while (i <= middle) {
+            listings[k] = temp[i];
+            ++k;
+            ++i;
         }
     }
+    if (criteria == "Odometer") {
+        while (i <= middle && j <= high) {
+            if (temp[i]->Odometer <= temp[j]->Odometer) {
+                listings[k] = temp[i];
+                ++i;
+            } else {
+                listings[k] = temp[j];
+                ++j;
+            }
+            ++k;
+        }
+        while (i <= middle) {
+            listings[k] = temp[i];
+            ++k;
+            ++i;
+        }
+    }
+    if (criteria == "Year") {
+        while (i <= middle && j <= high) {
+            if (temp[i]->Year >= temp[j]->Year) {
+                listings[k] = temp[i];
+                ++i;
+            } else {
+                listings[k] = temp[j];
+                ++j;
+            }
+            ++k;
+        }
+        while (i <= middle) {
+            listings[k] = temp[i];
+            ++k;
+            ++i;
+        }
+    }
+    if (criteria == "Reliability") {
+        while (i <= middle && j <= high) {
+            if (temp[i]->Reliability >= temp[j]->Reliability) {
+                listings[k] = temp[i];
+                ++i;
+            } else {
+                listings[k] = temp[j];
+                ++j;
+            }
+            ++k;
+        }
+        while (i <= middle) {
+            listings[k] = temp[i];
+            ++k;
+            ++i;
+        }
+    }
+    if (criteria == "Rank") {
+        while (i <= middle && j <= high) {
+            if (temp[i]->Rank >= temp[j]->Rank) {
+                listings[k] = temp[i];
+                ++i;
+            } else {
+                listings[k] = temp[j];
+                ++j;
+            }
+            ++k;
+        }
+        while (i <= middle) {
+            listings[k] = temp[i];
+            ++k;
+            ++i;
+        }
+    }
+}
 void swap(Vehicle* a, Vehicle* b){
     Vehicle temp = *a;
     *a = *b;
@@ -162,17 +164,17 @@ int pivotSwap (vector<Vehicle*>& listings, int low, int high, string criteria){
                 swap(listings[x], listings[i]);
             }
         if(criteria == "Year")
-            if (listings[i]->Year < pivot->Year) {
+            if (listings[i]->Year > pivot->Year) {
                 x++;
                 swap(listings[x], listings[i]);
             }
         if(criteria == "Reliability")
-            if (listings[i]->Reliability < pivot->Reliability) {
+            if (listings[i]->Reliability > pivot->Reliability) {
                 x++;
                 swap(listings[x], listings[i]);
             }
         if(criteria == "Rank")
-            if (listings[i]->Rank < pivot->Rank) {
+            if (listings[i]->Rank > pivot->Rank) {
                 x++;
                 swap(listings[x], listings[i]);
             }
@@ -196,23 +198,22 @@ void mergeSort(vector<Vehicle*>& listings, int size, int low, int high, string c
     }
 }
 void updateRank(vector<Vehicle*>& listings, int multiplier){
-    int rank;
     int size = listings.size();
     for(int i = 0; i < size; i++){
-        if(i < size*0.01){
-            listings[i]->Rank  += 5 * multiplier;
+        if(i <= size*0.01){
+            listings[i]->Rank  += (5 * multiplier);
         }
-        else if(i > size*0.01 && i <  size*0.05){
-            listings[i]->Rank  += 4 * multiplier;
+        else if(i > size*0.01 && i <=  size*0.05){
+            listings[i]->Rank  += (4 * multiplier);
         }
-        else if(i > size*0.05 && i <  size*0.1){
-            listings[i]->Rank  += 3 * multiplier;
+        else if(i > size*0.05 && i <= size*0.1){
+            listings[i]->Rank  += (3 * multiplier);
         }
-        else if(i > size*0.1 && i <  size*0.25){
-            listings[i]->Rank  += 2 * multiplier;
+        else if(i > size*0.1 && i <= size*0.25){
+            listings[i]->Rank  += (2 * multiplier);
         }
         else {
-            listings[i]->Rank += 1 * multiplier;
+            listings[i]->Rank += (1 * multiplier);
         }
     }
 }
@@ -232,7 +233,7 @@ int main() {
     // Creates 2nd window
     WINDOW *inputWin = newwin(8, 80, 11, 0);
     keypad(inputWin, TRUE);
-    scrollok(inputWin, TRUE);
+    scrollok(main, TRUE);
 
     vector<string> choices;
     vector<string> priority;
@@ -243,120 +244,165 @@ int main() {
     bool prioritySet = false;
     int userOption = 0;
 
+    attron(A_STANDOUT);
+    printw("THE CAR GURUS\n");
+    attroff(A_STANDOUT);
     printw("You might not know what you want, but we can help you.\n");
     printw("Click any key to start the process of finding your next dream car...");
     getch();
     clear();
 
-    while(!prioritySet) {
-        choices.emplace_back("Price");
-        choices.emplace_back("Year");
-        choices.emplace_back("Odometer");
-        choices.emplace_back("Reliability");
-
-        while (priority.size() != 4) {
-            printw("Rank these criterias from most important to least important\n");
-            userOption = displayMenu(choices, userOption, 1, main);
-            priority.emplace_back(choices[userOption]);
-            choices.erase(choices.begin() + userOption);
-            clear();
-        }
-
-        printw("Your Ranking\n");
-        string line;
-        for(int i = 0; i < 4; i++) {
-            line = to_string(i + 1) + ". " + priority[i];
-            mvwprintw(main, i + 1, 0, line.c_str());
-        }
-
-        mvwprintw(main, 7, 0, "Does this ranking look right?");
-        choices.emplace_back("1. Yes");
-        choices.emplace_back("2. No");
-        userOption = displayMenu(choices, userOption, 8, main);
-        choices.clear();
-        clear();
-
-        if(userOption == 0) {
-            prioritySet = true;
-        }
-        else if(userOption == 1) {
-            priority.clear();
-        }
-    }
-
-    printw("Which sorting algorithm would you like to run?");
-    choices.emplace_back("1. Merge Sort");
-    choices.emplace_back("2. Quick Sort");
-    userOption = displayMenu(choices, userOption, 1, main);
-
     while(true) {
-        try {
-            mvprintw(4, 0, "What is the number of potential cars you want to return (Max 50)? ");
-            char input[3];
-            clrtoeol();
-            getnstr(input, 3);
-            numOfPrintedCars = stoi(input);
-            if (numOfPrintedCars < 1 || numOfPrintedCars > 50) {
-                throw invalid_argument("That is an invalid number. Please try again.");
-            } else {
-                printw("Processing has begun...");
-                wrefresh(main);
-                break;
+        while (!prioritySet) {
+            choices.emplace_back("Price");
+            choices.emplace_back("Year");
+            choices.emplace_back("Odometer");
+            choices.emplace_back("Reliability");
+
+            while (priority.size() != 4) {
+                printw("Rank these criterias from most important to least important\n");
+                userOption = displayMenu(choices, userOption, 1, main);
+                priority.emplace_back(choices[userOption]);
+                choices.erase(choices.begin() + userOption);
+                clear();
+            }
+
+            printw("Your Ranking\n");
+            string line;
+            for (int i = 0; i < 4; i++) {
+                line = to_string(i + 1) + ". " + priority[i];
+                mvwprintw(main, i + 1, 0, line.c_str());
+            }
+
+            mvwprintw(main, 7, 0, "Does this ranking look right?");
+            choices.emplace_back("1. Yes");
+            choices.emplace_back("2. No");
+            userOption = displayMenu(choices, userOption, 8, main);
+            choices.clear();
+            clear();
+
+            if (userOption == 0) {
+                prioritySet = true;
+            } else if (userOption == 1) {
+                priority.clear();
             }
         }
-        catch (invalid_argument& error) {
-            attron(A_BOLD);
-            clrtobot();
-            mvprintw(5, 0, error.what());
-            attroff(A_BOLD);
-        }
-    }
 
-    if(userOption == 0) {
-        auto start = std::chrono::high_resolution_clock::now();
-        for(int i = 0; i < 4; i++) {
-            mergeSort(listings, numOfCars, 0, numOfCars - 1, priority[i]);
-            updateRank(listings, 4 - i);
-            string percentageComplete = to_string((i+1)*20) + " Percent Complete.";
-            mvprintw(5, 0, percentageComplete.c_str());
-            wrefresh(main);
+        printw("Which sorting algorithm would you like to run?");
+        choices.emplace_back("1. Merge Sort");
+        choices.emplace_back("2. Quick Sort");
+        userOption = displayMenu(choices, userOption, 1, main);
+        choices.clear();
+
+        while (true) {
+            try {
+                mvprintw(4, 0, "What is the number of potential cars you want to return (Max 50)? ");
+                char input[2];
+                clrtoeol();
+                getnstr(input, 2);
+                numOfPrintedCars = stoi(input);
+                if (numOfPrintedCars < 1 || numOfPrintedCars > 50) {
+                    throw invalid_argument("That is an invalid number. Please try again.");
+                } else {
+                    clear();
+                    printw("Processing has begun...");
+                    wrefresh(main);
+                    break;
+                }
+            }
+            catch (invalid_argument &error) {
+                attron(A_BOLD);
+                clrtobot();
+                mvprintw(5, 0, error.what());
+                attroff(A_BOLD);
+            }
         }
-        mergeSort(listings, numOfCars, 0, numOfCars - 1, "Rank");
-        mvprintw(5, 0, "100 Percent Complete.");
-        wrefresh(main);
-        auto finish = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = finish - start;
+
+        if (userOption == 0) {
+            auto start = std::chrono::high_resolution_clock::now();
+            for (int i = 0; i < 4; i++) {
+                mergeSort(listings, numOfCars, 0, numOfCars - 1, priority[i]);
+                updateRank(listings, 4 - i);
+                string percentageComplete = to_string((i + 1) * 20) + " Percent Complete....";
+                mvprintw(0, 0, percentageComplete.c_str());
+                wrefresh(main);
+            }
+            mergeSort(listings, numOfCars, 0, numOfCars - 1, "Rank");
+            mvprintw(0, 0, "100 Percent Complete....");
+            wrefresh(main);
+            auto finish = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = finish - start;
+            clear();
+
+            string priorityList =
+                    "Your priority list (from most important to least): " + priority[0] + ", " + priority[1] + ", " +
+                    priority[2] + ", " + priority[3] + "\n";
+            string time = "Using the merge sort alogirthm, your list of potential cars was generated in " +
+                          to_string(elapsed.count()) + " seconds";
+            printw(priorityList.c_str());
+            printw(time.c_str());
+        } else if (userOption == 1) {
+            auto start = std::chrono::high_resolution_clock::now();
+            for (int i = 0; i < 4; i++) {
+                quickSort(listings, 0, numOfCars - 1, priority[i]);
+                updateRank(listings, 4 - i);
+                string percentageComplete = to_string((i + 1) * 20) + " Percent Complete....";
+                mvprintw(0, 0, percentageComplete.c_str());
+                wrefresh(main);
+            }
+            quickSort(listings, 0, numOfCars - 1, "Rank");
+            mvprintw(0, 0, "100 Percent Complete....");
+            wrefresh(main);
+            auto finish = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = finish - start;
+            clear();
+
+            string priorityList =
+                    "Your priority list (from most important to least): " + priority[0] + ", " + priority[1] + ", " +
+                    priority[2] + ", " + priority[3] + "\n";
+            string time = "Using the quick sort alogirthm, your list of potential cars was generated in " +
+                          to_string(elapsed.count()) + " seconds";
+            printw(priorityList.c_str());
+            printw(time.c_str());
+        }
+
+        for (int i = 0; i < numOfPrintedCars; i++) {
+            Vehicle *car = listings[i];
+            ostringstream stream;
+            stream << std::fixed << std::setprecision(2) << car->Reliability;
+            string place =  to_string(i + 1) + ".";
+            string rank = "Rank(Out of 50): " + to_string(car->Rank);
+            string year = "Year: " + to_string(car->Year);
+            string make = "Make: " + car->make;
+            string model = "Model: " + car->model;
+            string odo = "Odometer: " + to_string(car->Odometer);
+            string price = "Price: $" + to_string(car->Price);
+            string reliability = "Reliability(Out of 5): " + stream.str();
+            mvprintw(i + 3, 0, "%-5s%-23s%-14s%-23s%-20s%-20s%-20s%-25s\n", place.c_str(), rank.c_str(), year.c_str(), make.c_str(), model.c_str(), odo.c_str(), price.c_str(), reliability.c_str());
+        }
+
+        getch();
         clear();
 
-        string title = "Using the merge sort alogirthm, your list of potential cars was generated in " + to_string(elapsed.count()) + " seconds";
-        printw(title.c_str());
-    }
-    else if(userOption == 1) {
-        auto start = std::chrono::high_resolution_clock::now();
-        for(int i = 0; i < 4; i++) {
-            quickSort(listings, 0, numOfCars - 1, priority[i]);
-            updateRank(listings, 4 - i);
-            string percentageComplete = to_string((i+1)*20) + "Percent Complete.";
-            mvprintw(5, 0, percentageComplete.c_str());
-            wrefresh(main);
+        printw("Do you want to run another search?");
+        choices.emplace_back("1. Yes");
+        choices.emplace_back("2. No");
+        userOption = displayMenu(choices, userOption, 1, main);
+        choices.clear();
+
+        if(userOption == 0) {
+            clear();
+            prioritySet = false;
+            priority.clear();
+            continue;
         }
-        quickSort(listings, 0, numOfCars - 1, "Rank");
-        mvprintw(5, 0, "100 Percent Complete.");
-        wrefresh(main);
-        auto finish = std::chrono::high_resolution_clock::now();
-        auto elapsed = finish - start;
-        clear();
-
-        string title = "Using the quick sort alogirthm, your list of potential cars was generated in " + to_string(elapsed.count()) + " seconds";
-        printw(title.c_str());
+        else if(userOption == 1) {
+            clear();
+            break;
+        }
     }
 
-    for(int i = 0; i < numOfPrintedCars; i++) {
-        Vehicle* car = listings[i];
-        string carInfo = to_string(i+1) + ". " + to_string(car->Rank) + " " + to_string(car->Year) + " " + car->make + " " + car->model + " " + to_string(car->Odometer);
-        mvprintw(i + 2, 0, carInfo.c_str());
-    }
-    getch();
+    endwin();
 }
 
 int displayMenu(vector<string>& choices, int highlight, int y, WINDOW* window) {
